633f9bbb160f2c411efeffb8102769ed4e3c8136
Dantiff <danielmburu674@gmail.com>
2017-06-01 18:32:46 +0000

    save messages to DB

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.core.mail import send_mail
 from django.views import View
 from portfolio.forms import *
 from portfolio.models import *
@@ -14,13 +15,27 @@ class Index(View):
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
         if form.is_valid():
-            message = Message(
+
             name=form.cleaned_data['name'],
             email=form.cleaned_data['email'],
             subject=form.cleaned_data['subject'],
             message=form.cleaned_data['message'],
+
+            message = Message(
+            name=name,
+            email=email,
+            subject=subject,
+            message=message,
             )
             message.save()
+
+            #send_mail(
+            #    subject,
+            #    message,
+            #    email,
+            #    ['danielmburu674@gmail.com'],
+            #    fail_silently=False,
+            #)
             return HttpResponseRedirect('/')
 
         return render(request, self.template_name, {'form': form})
