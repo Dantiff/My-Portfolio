6c3803ea981272942eab30218c91597261224014
Dantiff <danielmburu674@gmail.com>
2017-06-01 16:15:18 +0000

    extend base template

--- static/js/custom.js
+++ static/js/custom.js
@@ -30,38 +30,47 @@ $(document).ready(function() {
 
   /* Owl Carousel
   -----------------------------------------------*/
-  $(document).ready(function() {
-    $("#projects-slider").owlCarousel({
+$(document).ready(function() {
+
+    $("#blogs-slider").owlCarousel({
       autoPlay: 6000,
-      items : 1,
-      itemsDesktop : [1199,2],
-      itemsDesktopSmall : [979,1],
-      itemsTablet: [768,1],
-      itemsTabletSmall: [985,2],
-      itemsMobile : [500,1],
-      navigation:true,
-      navigationText: [
-        "<i class='fa fa-chevron-circle-left fa-2x'></i>",
-        "<i class='fa fa-chevron-circle-right fa-2x'></i>"
-      ],
+      items : 1, //10 items above 1000px browser width
+      itemsDesktop : [1000,1], //5 items between 1000px and 901px
+      itemsDesktopSmall : [900,2], // betweem 900px and 601px
+      itemsTablet: [600,1], //2 items between 600 and 0
+      itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
     });
 
+});
+
+$(document).ready(function() {
 
     $("#tutorials-slider").owlCarousel({
       autoPlay: 6000,
-      items : 1,
-      itemsDesktop : [1199,2],
-      itemsDesktopSmall : [979,1],
-      itemsTablet: [768,1],
-      itemsTabletSmall: [985,2],
-      itemsMobile : [500,1],
-      navigation:true,
-      navigationText: [
-        "<i class='fa fa-chevron-circle-left fa-2x'></i>",
-        "<i class='fa fa-chevron-circle-right fa-2x'></i>"
-      ],
+      items : 1, //10 items above 1000px browser width
+      itemsDesktop : [1000,1], //5 items between 1000px and 901px
+      itemsDesktopSmall : [900,2], // betweem 900px and 601px
+      itemsTablet: [600,1], //2 items between 600 and 0
+      itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
     });
-  });
+
+
+});
+
+$(document).ready(function() {
+
+
+    $("#projects-slider").owlCarousel({
+      autoPlay: 6000,
+      items : 1, //10 items above 1000px browser width
+      itemsDesktop : [1000,1], //5 items between 1000px and 901px
+      itemsDesktopSmall : [900,2], // betweem 900px and 601px
+      itemsTablet: [600,1], //2 items between 600 and 0
+      itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+    });
+
+});
+
 
 
   /* Back top
@@ -86,3 +95,114 @@ $(document).ready(function() {
 
   });
 
+
+  /* Custom Navigation
+  -------------------------------*/
+
+(function($) {
+
+	skel.breakpoints({
+		xlarge: '(max-width: 1680px)',
+		large: '(max-width: 1280px)',
+		medium: '(max-width: 1024px)',
+		small: '(max-width: 736px)',
+		xsmall: '(max-width: 480px)'
+	});
+
+	$(function() {
+
+		var $body = $('body'),
+			$header = $('#header'),
+			$nav = $('#nav'), $nav_a = $nav.find('a'),
+			$wrapper = $('#wrapper');
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+
+		// Header.
+			var ids = [];
+
+			// Set up nav items.
+				$nav_a
+					.scrolly({ offset: 44 })
+					.on('click', function(event) {
+
+						var $this = $(this),
+							href = $this.attr('href');
+
+						// Not an internal link? Bail.
+							if (href.charAt(0) != '#')
+								return;
+
+						// Prevent default behavior.
+							event.preventDefault();
+
+						// Remove active class from all links and mark them as locked (so scrollzer leaves them alone).
+							$nav_a
+								.removeClass('active')
+								.addClass('scrollzer-locked');
+
+						// Set active class on this link.
+							$this.addClass('active');
+
+					})
+					.each(function() {
+
+						var $this = $(this),
+							href = $this.attr('href'),
+							id;
+
+						// Not an internal link? Bail.
+							if (href.charAt(0) != '#')
+								return;
+
+						// Add to scrollzer ID list.
+							id = href.substring(1);
+							$this.attr('id', id + '-link');
+							ids.push(id);
+
+					});
+
+			// Initialize scrollzer.
+				$.scrollzer(ids, { pad: 300, lastHack: true });
+
+		// Off-Canvas Navigation.
+
+			// Title Bar.
+				$(
+					'<div id="titleBar">' +
+						'<a href="#header" class="toggle"></a>' +
+						'<span class="title">' + $('#logo').html() + '</span>' +
+					'</div>'
+				)
+					.appendTo($body);
+
+			// Header.
+				$('#header')
+					.panel({
+						delay: 500,
+						hideOnClick: true,
+						hideOnSwipe: true,
+						resetScroll: true,
+						resetForms: true,
+						side: 'right',
+						target: $body,
+						visibleClass: 'header-visible'
+					});
+
+			// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
+				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
+					$('#titleBar, #header, #wrapper')
+						.css('transition', 'none');
+
+	});
+
+})(jQuery);
